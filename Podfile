ce99b5464f6679cfa7df6730cffee1624d46d537
Damian Markowski <contact@damianmarkowski.pl>
2018-07-28 22:02:59 +0100

    snapshot tests configuration added

---
+++ Podfile
@@ -0,0 +1,7 @@
+use_frameworks!
+platform :ios, '11.0'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'UICollectionViewRowsColsSnapshotTests' do
+	pod 'iOSSnapshotTestCase'
+end
---
+++ UICollectionViewRowsColsSnapshotTests/Screens/Start/StartViewControllerSnapshotTests.swift
@@ -0,0 +1,23 @@
+//
+//  StartViewControllerSnapshotTests.swift
+//  UICollectionViewRowsColsSnapshotTests
+//
+//  Created by Damian Markowski on 28.07.2018.
+//  Copyright © 2018 Damian Markowski. All rights reserved.
+//
+
+import XCTest
+import iOSSnapshotTestCase
+@testable import UICollectionViewRowsColsTests
+
+class StartViewControllerSnapshotTests: iOSSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
