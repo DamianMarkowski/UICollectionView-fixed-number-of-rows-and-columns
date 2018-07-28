9fc7b8c70761590378ed129ea2cf3200004b219c
Damian Markowski <contact@damianmarkowski.pl>
2018-07-28 22:38:49 +0100

    unit test for number cell added

---
+++ UICollectionViewRowsColsTests/Screens/Start/ViewComponents/NumberCellTests.swift
@@ -0,0 +1,32 @@
+//
+//  NumberCellTests.swift
+//  UICollectionViewRowsColsTests
+//
+//  Created by Damian Markowski on 28.07.2018.
+//  Copyright © 2018 Damian Markowski. All rights reserved.
+//
+
+import XCTest
+@testable import UICollectionViewRowsCols
+
+class NumberCellTests: XCTestCase {
+    
+    var sut: NumberCell!
+    
+    override func setUp() {
+        super.setUp()
+        let bundle = Bundle(for: NumberCell.self)
+        let nib = bundle.loadNibNamed("NumberCell", owner: nil, options: nil)
+        sut = nib?.first as? NumberCell
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+    }
+    
+    func test_init_numberLabel_shouldNotBeNil() {
+        XCTAssertNotNil(sut.numberLabel)
+    }
+    
+}
